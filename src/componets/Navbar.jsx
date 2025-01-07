49f1090297e16ef8b5058a984d55182b88ca4297
3laa <3laa.r.812@gmail.com>
2025-01-08 00:52:09 +0200

    modified bg navbar

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             </div>
         </div>
         {mobileDrawerOpen && (
-          <div className="fixed right-0 z-20 bg-netural-900 w-full p-12 
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 
           flex flex-col justify-center items-center lg:hidden">
             <ul>
             {navItems.map((item, index)=> (
